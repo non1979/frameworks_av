69309cd0444f47162e7d1df660633e4fd66cf454
non1979 <artamonov@gmail.com>
2019-04-09 05:59:12 +0300

    Revert "DO NOT MERGE: defensive parsing of mp3 album art information"
    This reverts commit dfb9b7f674d660cb3827cb5a2a3702bc89370b27.

--- media/libstagefright/id3/ID3.cpp
+++ media/libstagefright/id3/ID3.cpp
@@ -837,21 +837,20 @@ void ID3::Iterator::findFrame() {
     }
 }
 
-// return includes terminator;  if unterminated, returns > limit
-static size_t StringSize(const uint8_t *start, size_t limit, uint8_t encoding) {
-
+static size_t StringSize(const uint8_t *start, uint8_t encoding) {
     if (encoding == 0x00 || encoding == 0x03) {
         // ISO 8859-1 or UTF-8
-        return strnlen((const char *)start, limit) + 1;
+        return strlen((const char *)start) + 1;
     }
 
     // UCS-2
     size_t n = 0;
-    while ((n+1 < limit) && (start[n] != '\0' || start[n + 1] != '\0')) {
+    while (start[n] != '\0' || start[n + 1] != '\0') {
         n += 2;
     }
-    n += 2;
-    return n;
+
+    // Add size of null termination.
+    return n + 2;
 }
 
 const void *
@@ -872,16 +871,8 @@ ID3::getAlbumArt(size_t *length, String8 *mime) const {
 
         if (mVersion == ID3_V2_3 || mVersion == ID3_V2_4) {
             uint8_t encoding = data[0];
-            size_t consumed = 1;
-
-            // *always* in an 8-bit encoding
-            size_t mimeLen = StringSize(&data[consumed], size - consumed, 0x00);
-            if (mimeLen > size - consumed) {
-                ALOGW("bogus album art size: mime");
-                return NULL;
-            }
-            mime->setTo((const char *)&data[consumed]);
-            consumed += mimeLen;
+            mime->setTo((const char *)&data[1]);
+            size_t mimeLen = strlen((const char *)&data[1]) + 1;
 
 #if 0
             uint8_t picType = data[1 + mimeLen];
@@ -892,29 +883,19 @@ ID3::getAlbumArt(size_t *length, String8 *mime) const {
             }
 #endif
 
-            consumed++;
-            if (consumed >= size) {
-                ALOGW("bogus album art size: pic type");
+            size_t descLen = StringSize(&data[2 + mimeLen], encoding);
+
+            if (size < 2 ||
+                    size - 2 < mimeLen ||
+                    size - 2 - mimeLen < descLen) {
+                ALOGW("bogus album art sizes");
                 return NULL;
             }
+            *length = size - 2 - mimeLen - descLen;
 
-            size_t descLen = StringSize(&data[consumed], size - consumed, encoding);
-            consumed += descLen;
-
-            if (consumed >= size) {
-                ALOGW("bogus album art size: description");
-                return NULL;
-            }
-
-            *length = size - consumed;
-
-            return &data[consumed];
+            return &data[2 + mimeLen + descLen];
         } else {
             uint8_t encoding = data[0];
-
-            if (size <= 5) {
-                return NULL;
-            }
 
             if (!memcmp(&data[1], "PNG", 3)) {
                 mime->setTo("image/png");
@@ -935,10 +916,7 @@ ID3::getAlbumArt(size_t *length, String8 *mime) const {
             }
 #endif
 
-            size_t descLen = StringSize(&data[5], size - 5, encoding);
-            if (descLen > size - 5) {
-                return NULL;
-            }
+            size_t descLen = StringSize(&data[5], encoding);
 
             *length = size - 5 - descLen;
 
